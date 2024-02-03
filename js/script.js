20b3b0a62b0e7f6fb84c472c7ec6df04c0ec499d
Deepika Rajan <147830675+DEEPIKARAJAN-E@users.noreply.github.com>
2024-02-03 23:51:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function changecontent() {
+   let x=document.getElementById("know-more");
+   if(x.style.display=="block")
+   {
+    x.style.display="none";
+   }
+   else{
+    x.style.display="block";
+   } 
+}
+
